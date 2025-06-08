79aa9295e48f9ed39b6c889ee29542e34326fbca
ησᴛ ᴄσᴅєʀ <shamansh3137@gmail.com>
2025-06-09 01:00:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,10 +19,10 @@ LOGGER_ID = int(getenv("LOGGER_ID", -1002141133985))
 OWNER_ID = int(getenv("OWNER_ID", 8143754205))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TEAMPURVI/Sonali-MusicV2",)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Im-Notcoder/Sonali-Music",)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", ".....")  
+    "GIT_TOKEN", "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")  
 # 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 API_KEY = getenv("API_KEY", 'NxGBNexGenBotsad3e6e') # youtube song api key, get it from ht
