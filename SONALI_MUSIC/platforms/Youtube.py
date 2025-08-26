7bd27955f61ccf21df0a7c0a24c12236c7459199
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-26 16:26:29 +0530

    Update Youtube.py

--- SONALI_MUSIC/platforms/Youtube.py
+++ SONALI_MUSIC/platforms/Youtube.py
@@ -19,11 +19,8 @@ import config
 
 from os import getenv
 
-#API_URL = getenv("API_URL", 'https://api.thequickearn.xyz')
-#API_KEY = getenv("API_KEY", 'NxGBNexGenBots9189cf')
-
-API_URL = getenv("API_URL", 'https://bitflow.in/api/youtube')
-API_KEY = getenv("API_KEY", '1spiderkey2')
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots9189cf')
 
 def cookie_txt_file():
     cookie_dir = f"{os.getcwd()}/cookies"
