727c4c492182e635b3e110e6438ed18c9edb67da
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-26 19:22:34 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -108,6 +108,27 @@ def stream_markup(_, chat_id):
     return buttons
                 
 
+def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
+                
 
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
     buttons = [
