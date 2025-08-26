162ccd0fdc88c439c5f39c176374505e66693555
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-27 01:07:25 +0530

    Update gadd.py

--- SONALI_MUSIC/plugins/tools/gadd.py
+++ SONALI_MUSIC/plugins/tools/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "8143754205"
+OWNERS = "7473021518"
 from SONALI_MUSIC import app
 from SONALI_MUSIC.utils.database import add_served_chat, get_assistant
 
