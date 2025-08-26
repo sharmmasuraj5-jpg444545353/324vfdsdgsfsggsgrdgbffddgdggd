2166d1614fbb54c3472feeec9de42b686439c101
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-26 19:00:00 +0530

    Update queue.py

--- SONALI_MUSIC/utils/inline/queue.py
+++ SONALI_MUSIC/utils/inline/queue.py
@@ -67,19 +67,20 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(
+                text="• ᴊᴏɪη ησᴡ •",
+                url=f"https://t.me/ONE_WAS_SIGMA"
+            ),
+            InlineKeyboardButton(
+                text="• ɢʀσᴜᴘ ᴄʜᴧᴛ •",
+                url="https://t.me/+gMy8Cp190ediNzZl"
+            ),
         ],
         [
             InlineKeyboardButton(
-                text="˹❍ᴡηєʀ ᴀʙσᴜᴛ˼ ", url=f"https://t.me/ONE_WAS_SIGMA"
-            ),
-            InlineKeyboardButton(
-                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/+gMy8Cp190ediNzZl"
-            ),
+                text="✙ ʌᴅᴅ ϻє ɪη ʏσυʀ ɢʀσυᴘ ✙",
+                url=f"https://t.me/{app.username}?startgroup=true"
+            )
         ],
-      ]
+    ]
     return buttons
