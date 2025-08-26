d49161ab98b8f1f7abf04e769d3bf6356701b2f0
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-26 17:33:04 +0530

    Update info.py

--- SONALI_MUSIC/plugins/mics/info.py
+++ SONALI_MUSIC/plugins/mics/info.py
@@ -1,10 +1,13 @@
 import asyncio
-from pyrogram import filters, Client, enums
+from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from SONALI_MUSIC import app
 
 INFO_TEXT = """
-<u><b>ᴜsᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b></u>
+<u><b>👤 ᴜsᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b></u>
+
+<b>● ᴘʀᴏғɪʟᴇ ᴘɪᴄ ➠</b> [ᴄʟɪᴄᴋ ʜᴇʀᴇ]({})
+
 <b>● ᴜsᴇʀ ɪᴅ ➠</b> <code>{}</code>
 <b>● ᴜsᴇʀɴᴀᴍᴇ ➠</b> <code>@{}</code>
 <b>● ᴍᴇɴᴛɪᴏɴ ➠</b> {}
@@ -35,15 +38,11 @@ async def userinfo(_, message: Message):
     user_id = message.from_user.id
 
     try:
-        # /info <user_id or username>
         if not message.reply_to_message and len(message.command) == 2:
             user_id = message.text.split(None, 1)[1]
-
-        # if reply
         elif message.reply_to_message:
             user_id = message.reply_to_message.from_user.id
 
-        # get user info
         user_info = await app.get_chat(user_id)
         user = await app.get_users(user_id)
         status = await userstatus(user.id)
@@ -53,26 +52,35 @@ async def userinfo(_, message: Message):
         username = user_info.username or "N/A"
         mention = user.mention
 
-        # Choose profile link based on availability of username
         if user.username:
             profile_url = f"https://t.me/{user.username}"
         else:
             profile_url = f"tg://user?id={user.id}"
 
+        # Get profile photo link
+        photos = await app.get_profile_photos(user.id, limit=1)
+        if photos:
+            # upload photo in current chat invisibly
+            sent = await app.send_photo(chat_id, photos[0].file_id, caption=".")
+            photo_link = sent.photo.file_id  
+            photo_link = f"https://t.me/c/{str(chat_id)[4:]}/{sent.id}"
+            await sent.delete() 
+        else:
+            photo_link = profile_url
+
         await app.send_message(
             chat_id,
-            text=INFO_TEXT.format(id, username, mention, status, dc_id),
+            text=INFO_TEXT.format(photo_link, id, username, mention, status, dc_id),
             reply_to_message_id=message.id,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("👤 ᴜsᴇʀ ᴘʀᴏғɪʟᴇ", url=profile_url)
-                    ],
-                    [
+                        InlineKeyboardButton(f"{user.first_name}", url=profile_url),
                         InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
                     ]
                 ]
             ),
+            disable_web_page_preview=True
         )
     except Exception as e:
         await message.reply_text(str(e))
