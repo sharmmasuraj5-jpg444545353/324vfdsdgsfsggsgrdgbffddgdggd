cca81d00bc43bace5019743f2180c37b1863cf30
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-27 14:55:19 +0530

    Create vcmem.py

---
+++ SONALI_MUSIC/plugins/purvi/vcmem.py
@@ -0,0 +1,81 @@
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from strings import get_string
+from SONALI_MUSIC import app
+from SONALI_MUSIC.utils import SonaBin
+
+from SONALI_MUSIC.utils.database import get_assistant, get_lang
+import asyncio
+from os import getenv
+from dotenv import load_dotenv
+from pyrogram import Client
+from dotenv import load_dotenv
+import config
+
+from SONALI_MUSIC.logging import LOGGER
+from SONALI_MUSIC.utils.database import (
+    delete_filter,
+    get_cmode,
+    get_lang,
+    is_active_chat,
+    is_commanddelete_on,
+    is_maintenance,
+    is_nonadmin_chat,
+    set_loop,
+)
+from SONALI_MUSIC.core.call import Sona
+
+
+@app.on_message(
+    filters.command(["vcuser", "vcusers", "vcmember", "vcmembers", "cu", "cm"], prefixes=["/", "!", ".", "V", "v"]) & filters.admin
+)
+async def vc_members(client, message):
+    try:
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+    except:
+        _ = get_string("en")
+    msg = await message.reply_text(_["V_C_1"])
+    userbot = await get_assistant(message.chat.id)
+    TEXT = ""
+    try:
+        async for m in userbot.get_call_members(message.chat.id):
+            chat_id = m.chat.id
+            username = m.chat.username
+            is_hand_raised = m.is_hand_raised
+            is_video_enabled = m.is_video_enabled
+            is_left = m.is_left
+            is_screen_sharing_enabled = m.is_screen_sharing_enabled
+            is_muted = bool(m.is_muted and not m.can_self_unmute)
+            is_speaking = not m.is_muted
+
+            if m.chat.type != ChatType.PRIVATE:
+                title = m.chat.title
+            else:
+                try:
+                    title = (await client.get_users(chat_id)).mention
+                except:
+                    title = m.chat.first_name
+
+            TEXT += _["V_C_2"].format(
+                title,
+                chat_id,
+                username,
+                is_video_enabled,
+                is_screen_sharing_enabled,
+                is_hand_raised,
+                is_muted,
+                is_speaking,
+                is_left,
+            )
+            TEXT += "\n\n"
+        if len(TEXT) < 4000:
+            await msg.edit(TEXT or _["V_C_3"])
+        else:
+            link = await SonaBin(TEXT)
+            await msg.edit(
+                _["V_C_4"].format(link),
+                disable_web_page_preview=True,
+            )
+    except ValueError as e:
+        await msg.edit(_["V_C_5"])
