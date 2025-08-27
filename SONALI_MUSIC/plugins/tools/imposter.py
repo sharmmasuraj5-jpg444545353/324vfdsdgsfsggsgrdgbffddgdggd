c3b2e009fb07295a5d791ab199dc80d1c1674d9b
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-27 18:28:10 +0530

    Update imposter.py

--- SONALI_MUSIC/plugins/tools/imposter.py
+++ SONALI_MUSIC/plugins/tools/imposter.py
@@ -1,4 +1,5 @@
 from pyrogram import filters
+import random
 from pyrogram.types import Message
 from SONALI_MUSIC import app
 from SONALI_MUSIC.plugins.tools.pretenderdb import (
@@ -6,6 +7,27 @@ from SONALI_MUSIC.plugins.tools.pretenderdb import (
     add_userdata, get_userdata, usr_data
 )
 from SONALI_MUSIC.utils.admin_filters import admin_filter
+
+
+PURVI = [
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
 
 @app.on_message(filters.group & ~filters.bot & ~filters.via_bot, group=69)
 async def chk_usr(_, message: Message):
@@ -87,8 +109,11 @@ async def chk_usr(_, message: Message):
             message.from_user.last_name,
         )
     if msg != "":
-        await message.reply_photo("https://files.catbox.moe/jxribw.jpg", caption=msg)
-
+        photo = random.choice(PURVI)
+        buttons = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Add me in your group", url=f"https://t.me/{app.username}?startgroup=true")]]
+        )
+        await message.reply_photo(photo=photo, caption=msg, reply_markup=buttons)
 
 @app.on_message(filters.group & filters.command("imposter") & ~filters.bot & ~filters.via_bot & admin_filter)
 async def set_mataa(_, message: Message):
