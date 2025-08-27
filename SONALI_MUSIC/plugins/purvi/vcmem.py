02f837a697f58d6e602f4b72e8e6964b62c67765
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-27 15:07:16 +0530

    Update vcmem.py

--- SONALI_MUSIC/plugins/purvi/vcmem.py
+++ SONALI_MUSIC/plugins/purvi/vcmem.py
@@ -1,16 +1,19 @@
 from pyrogram import filters
-from pyrogram.enums import ChatType
+from pyrogram.enums import ChatType, ChatMemberStatus
 from strings import get_string
 from SONALI_MUSIC import app
 from SONALI_MUSIC.utils import SonaBin
 from SONALI_MUSIC.utils.database import get_assistant
 from SONALI_MUSIC.core.call import Sona
 
+
 @app.on_message(
     filters.command(
         ["vcuser", "vcusers", "vcmember", "vcmembers", "cu", "cm"], 
         prefixes=["/", "!", ".", "V", "v"]
-    ) & filters.admin
+    ) & filters.chat_member_status(
+        status=ChatMemberStatus.ADMINISTRATOR | ChatMemberStatus.OWNER
+    )
 )
 async def vc_members(client, message):
     try:
@@ -18,9 +21,11 @@ async def vc_members(client, message):
         _ = get_string(language)
     except:
         _ = get_string("en")
+
     msg = await message.reply_text(_["V_C_1"])
     userbot = await get_assistant(message.chat.id)
     TEXT = ""
+
     try:
         async for m in userbot.get_call_members(message.chat.id):
             chat_id = m.chat.id
@@ -52,6 +57,7 @@ async def vc_members(client, message):
                 is_left,
             )
             TEXT += "\n\n"
+
         if len(TEXT) < 4000:
             await msg.edit(TEXT or _["V_C_3"])
         else:
@@ -60,5 +66,5 @@ async def vc_members(client, message):
                 _["V_C_4"].format(link),
                 disable_web_page_preview=True,
             )
-    except ValueError as e:
+    except ValueError:
         await msg.edit(_["V_C_5"])
