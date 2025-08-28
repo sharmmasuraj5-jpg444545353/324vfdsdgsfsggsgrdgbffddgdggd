e3eed90ea9287f808fdd74fcd7d24c494db28c93
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-28 23:51:46 +0530

    Update calendar.py

--- SONALI_MUSIC/plugins/tools/calendar.py
+++ SONALI_MUSIC/plugins/tools/calendar.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageEnhance
 from io import BytesIO
 import aiohttp
 import calendar
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from SONALI_MUSIC import app
 
 async def make_carbon(code):
@@ -12,23 +13,19 @@ async def make_carbon(code):
         async with session.post(url, json={"code": code}) as resp:
             image_data = await resp.read()
 
-    
     carbon_image = Image.open(BytesIO(image_data))
 
-    
     enhancer = ImageEnhance.Brightness(carbon_image)
-    bright_image = enhancer.enhance(1.7)  
-
+    bright_image = enhancer.enhance(1.7)
 
     output_image = BytesIO()
-    bright_image.save(output_image, format='PNG', quality=95) 
+    bright_image.save(output_image, format='PNG', quality=95)
     output_image.name = "carbon.png"
     return output_image
 
 
 @app.on_message(filters.command("calendar", prefixes="/"))
 async def send_calendar(_, message):
-    # Extract the year from the command arguments
     command_parts = message.text.split(" ")
     if len(command_parts) == 2:
         try:
@@ -37,15 +34,23 @@ async def send_calendar(_, message):
             await message.reply("**ɪɴᴠᴀʟɪᴅ ʏᴇᴀʀ ꜰᴏʀᴍᴀᴛ.**\n\n**ᴜꜱᴇ** `/calendar <year>`")
             return
     else:
-        await message.reply("**ᴘʟᴇᴀꜱᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴠᴀʟɪᴅ ʏᴇᴀʀ ᴀꜰᴛᴇʀ** `/calendar` **ᴄᴏᴍᴍᴀɴᴅ.**")
+        await message.reply("**⋟ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴠᴀʟɪᴅ ʏᴇᴀʀ ᴀꜰᴛᴇʀ ᴄᴏᴍᴍᴀɴᴅ.**\n\n**⋟ ᴜsᴀɢᴇ :-** `calender 2025`")
         return
 
-    # Generate the calendar for the specified year
     cal = calendar.TextCalendar()
     full_year_calendar = cal.formatyear(year, 2, 1, 1, 3)
 
-    # Generate the Carbon image for the calendar
     carbon_image = await make_carbon(full_year_calendar)
 
-    # Send the image as a reply to the user
-    await app.send_photo(message.chat.id, carbon_image, caption=f"**ᴄᴀʟᴇɴᴅᴀʀ ꜰᴏʀ ᴛʜᴇ ʏᴇᴀʀ :-** {year}")
+    caption = f"**⋟ ᴄᴀʟᴇɴᴅᴀʀ ᴏғ ʏᴇᴀʀ :-** `{year}`\n\n**⋟ ʙʏ :-** <u>{app.mention}</u>"
+
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("✙ ʌᴅᴅ ϻє ɪη ʏσυʀ ɢʀσυᴘ ✙", url=f"https://t.me/{app.username}?startgroup=true")]]
+    )
+
+    await app.send_photo(
+        message.chat.id,
+        carbon_image,
+        caption=caption,
+        reply_markup=keyboard
+    )
