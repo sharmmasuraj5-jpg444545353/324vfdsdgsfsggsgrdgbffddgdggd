11a4deb728b9c7b26a3c715c5bade733e64dbc68
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-29 15:28:46 +0530

    Update mongochk.py

--- SONALI_MUSIC/plugins/mics/mongochk.py
+++ SONALI_MUSIC/plugins/mics/mongochk.py
@@ -1,27 +1,41 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pymongo import MongoClient
 import re
 from SONALI_MUSIC import app as Sona
 
-
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
-
 
 @Sona.on_message(filters.command("mongochk"))
 async def mongo_command(client, message: Message):
+
+    ADD_ME_BUTTON = InlineKeyboardMarkup(
+        [[InlineKeyboardButton(f"✙ ʌᴅᴅ ϻє ɪη ʏσυʀ ɢʀσυᴘ ✙", url=f"https://t.me/{app.username}?startgroup=true")]]
+    )
+
     if len(message.command) < 2:
-        await message.reply("Please enter your MongoDB URL after the command. Example: /mongochk your_mongodb_url")
+        await message.reply(
+            f"**⋟ ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴍᴏɴɢᴏ ᴜʀʟ ᴀꜰᴛᴇʀ ᴄᴏᴍᴍᴀɴᴅ.**\n\n**ᴇxᴀᴍᴘʟᴇ :-** /mongochk mongo_url`",
+            reply_markup=ADD_ME_BUTTON
+        )
         return
 
     mongo_url = message.command[1]
     if re.match(mongo_url_pattern, mongo_url):
         try:
-            # Attempt to connect to the MongoDB instance
-            client = MongoClient(mongo_url, serverSelectionTimeoutMS=5000)
-            client.server_info()  # Will cause an exception if connection fails
-            await message.reply("𝗠𝗼𝗻𝗴𝗼𝗗𝗕 𝗨𝗥𝗟 𝗶𝘀 𝘃𝗮𝗹𝗶𝗱 𝗮𝗻𝗱 𝗰𝗼𝗻𝗻𝗲𝗰𝘁𝗶𝗼𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹✅")
+            mongo_client = MongoClient(mongo_url, serverSelectionTimeoutMS=5000)
+            mongo_client.server_info()  # ⋟ ᴡɪʟʟ ᴄᴀᴜꜱᴇ ᴀɴ ᴇxᴄᴇᴘᴛɪᴏɴ ɪꜰ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ꜰᴀɪʟꜱ
+            await message.reply(
+                f"**⋟ ᴍᴏɴɢᴏᴅʙ ᴜʀʟ ɪꜱ ᴠᴀʟɪᴅ ᴀɴᴅ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ ✅**\n\n**⋟ ᴄʜᴇᴄᴋ ʙʏ :– {app.mention}**",
+                reply_markup=ADD_ME_BUTTON
+            )
         except Exception as e:
-            await message.reply(f"Failed to connect to MongoDB: {e}")
+            await message.reply(
+                f"**⋟ ꜰᴀɪʟᴇᴅ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ᴛᴏ ᴍᴏɴɢᴏᴅʙ :-** {e}\n\n**⋟ ᴄʜᴇᴄᴋ ʙʏ :– {app.mention}",
+                reply_markup=ADD_ME_BUTTON
+            )
     else:
-        await message.reply("𝗜𝗻𝘃𝗮𝗹𝗶𝗱 𝗠𝗼𝗻𝗴𝗼𝗗𝗕 𝗨𝗥𝗟 𝗳𝗼𝗿𝗺𝗮𝘁💔")
+        await message.reply(
+            f"**⋟ ɪɴᴠᴀʟɪᴅ ᴍᴏɴɢᴏᴅʙ ᴜʀʟ ꜰᴏʀᴍᴀᴛ 💔**\n\n**⋟ ᴄʜᴇᴄᴋ ʙʏ :– {app.mention}**",
+            reply_markup=ADD_ME_BUTTON
+        )
