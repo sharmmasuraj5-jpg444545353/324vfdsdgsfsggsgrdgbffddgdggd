45f36a3f6c5472ef3bf2a3fef3f9d0599f68fa9e
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-29 22:33:12 +0530

    Update unmute.py

--- SONALI_MUSIC/plugins/admins/unmute.py
+++ SONALI_MUSIC/plugins/admins/unmute.py
@@ -1,25 +1,38 @@
 from SONALI_MUSIC import app
-from pyrogram import filters,enums
-from pyrogram.types import ChatPermissions 
+from pyrogram import filters, enums
+from pyrogram.types import ChatPermissions
 from SONALI_MUSIC.utils.Sona_BAN import admin_filter
 
 @app.on_message(filters.command("unmuteall") & admin_filter)
-async def unmute_all(_,msg):
-    chat_id=msg.chat.id   
-    user_id=msg.from_user.id
-    x = 0
-    bot=await app.get_chat_member(chat_id,user_id)
-    bot_permission=bot.privileges.can_restrict_members==True 
-    if bot_permission:
-        banned_users = []
-        async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.RESTRICTED):
-            banned_users.append(m.user.id)       
-            try:
-                    await app.restrict_chat_member(chat_id,banned_users[x], ChatPermissions(can_send_messages=True,can_send_media_messages=True,can_send_polls=True,can_add_web_page_previews=True,can_invite_users=True))
-                    print(f"ᴜɴᴍᴜᴛɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs {m.user.mention}")
-                    x += 1
-                                        
-            except Exception as e:
-                print(e)
+async def unmute_all(_, msg):
+    chat_id = msg.chat.id
+    user_id = msg.from_user.id
+    
+
+    bot = await app.get_chat_member(chat_id, user_id)
+    if not (bot.privileges and bot.privileges.can_restrict_members):
+        return await msg.reply_text("⚠️ ɴᴏ ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ ᴜɴᴍᴜᴛᴇ ᴍᴇᴍʙᴇʀs")
+
+    count = 0
+    async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.RESTRICTED):
+        try:
+            await app.restrict_chat_member(
+                chat_id,
+                m.user.id,
+                ChatPermissions(
+                    can_send_messages=True,
+                    can_send_media_messages=True,
+                    can_send_polls=True,
+                    can_add_web_page_previews=True,
+                    can_invite_users=True
+                )
+            )
+            count += 1
+            print(f"✅ ᴜɴᴍᴜᴛᴇᴅ {m.user.mention}")
+        except Exception as e:
+            print(f"❌ {m.user.id} - {e}")
+
+    if count == 0:
+        await msg.reply_text("😶 ɴᴏ ᴍᴜᴛᴇᴅ ᴍᴇᴍʙᴇʀs ғᴏᴜɴᴅ.")
     else:
-        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
+        await msg.reply_text(f"🔊 ᴜɴᴍᴜᴛᴇᴅ **{count}** ᴍᴇᴍʙᴇʀs ɪɴ ᴛʜɪs ᴄʜᴀᴛ ✅")
