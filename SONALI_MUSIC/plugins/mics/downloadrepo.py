7e3d893e84ba1dbd6431081adc16561a6e8ef188
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-29 15:21:03 +0530

    Create downloadrepo.py

---
+++ SONALI_MUSIC/plugins/mics/downloadrepo.py
@@ -0,0 +1,48 @@
+from pyrogram import Client, filters
+import git
+import shutil
+import os
+from SHUKLAMUSIC import app
+from threading import Thread
+
+@app.on_message(filters.command(["downloadrepo"]))
+def download_repo(_, message):
+    if len(message.command) != 2:
+        message.reply_text("**⋟ ᴘʀᴏᴠɪᴅᴇ ɢɪᴛʜᴜʙ ʀᴇᴘᴏ ᴜʀʟ ᴀꜰᴛᴇʀ ᴄᴏᴍᴍᴀɴᴅ.**\n\n**ᴇxᴀᴍᴘʟᴇ :-** `/downloadrepo Repo url`")
+        return
+
+    repo_url = message.command[1]
+    message.reply_text("**⋟ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴀɴᴅ ᴢɪᴘᴘɪɴɢ ᴛʜᴇ ʀᴇᴘᴏꜱɪᴛᴏʀʏ, ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ...**")
+    Thread(target=download_and_send, args=(message, repo_url)).start()
+
+def download_and_send(message, repo_url):
+    zip_path = download_and_zip_repo(repo_url)
+    if zip_path:
+        try:
+            with open(zip_path, "rb") as zip_file:
+                message.reply_document(zip_file)
+        except Exception as e:
+            message.reply_text(f"⋟ ꜰᴀɪʟᴇᴅ ᴛᴏ ꜱᴇɴᴅ ᴢɪᴘ: {e}")
+        finally:
+            if os.path.exists(zip_path):
+                os.remove(zip_path)
+    else:
+        message.reply_text("**⋟ ᴜɴᴀʙʟᴇ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ꜱᴘᴇᴄɪꜰɪᴇᴅ ɢɪᴛʜᴜʙ ʀᴇᴘᴏꜱɪᴛᴏʀʏ.**")
+
+def download_and_zip_repo(repo_url):
+    try:
+        repo_name = os.path.splitext(repo_url.split("/")[-1])[0]
+        repo_path = f"{repo_name}"
+        
+      
+        git.Repo.clone_from(repo_url, repo_path)
+        
+  
+        shutil.make_archive(repo_path, 'zip', repo_path)
+        return f"{repo_path}.zip"
+    except Exception as e:
+        print(f"**⋟ ᴇʀʀᴏʀ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴀɴᴅ ᴢɪᴘᴘɪɴɢ ɢɪᴛʜᴜʙ ʀᴇᴘᴏꜱɪᴛᴏʀʏ :-** {e}")
+        return None
+    finally:
+        if os.path.exists(repo_path):
+            shutil.rmtree(repo_path)
