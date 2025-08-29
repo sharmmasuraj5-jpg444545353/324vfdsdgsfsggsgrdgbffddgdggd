19d5b4994c4b2a3fb9353fc8f5471cabee1a1395
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-29 20:51:12 +0530

    Update unpinall.py

--- SONALI_MUSIC/plugins/admins/unpinall.py
+++ SONALI_MUSIC/plugins/admins/unpinall.py
@@ -2,37 +2,43 @@ from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from SONALI_MUSIC import app
 
-@app.on_callback_query(filters.regex(r"^unpin"))
+@app.on_callback_query()
 async def unpin_callback(client, CallbackQuery):
+    data = CallbackQuery.data
     user_id = CallbackQuery.from_user.id
     chat_id = CallbackQuery.message.chat.id
     member = await app.get_chat_member(chat_id, user_id)
 
     if member.status not in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER] or not member.privileges.can_pin_messages:
-        await CallbackQuery.answer("ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ʀɪɢʜᴛs ᴛᴏ ᴜɴᴘɪɴ, ʙᴋʟ", show_alert=True)
+        await CallbackQuery.answer("⚠ ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ʀɪɢʜᴛs ᴛᴏ ᴜɴᴘɪɴ, ʙᴋʟ", show_alert=True)
         return
 
-    msg_id = CallbackQuery.data.split("=")[1]
+    if data.startswith("delete_btn"):
+        await CallbackQuery.message.delete()
+        return
 
-    if msg_id == "yes":
-        await client.unpin_all_chat_messages(chat_id)
-        textt = "**✅ ᴀʟʟ ᴘɪɴɴᴇᴅ ᴍᴇssᴀɢᴇs ʜᴀᴠᴇ ʙᴇᴇɴ ᴜɴᴘɪɴɴᴇᴅ!**"
-    elif msg_id == "no":
-        textt = "**❌ ᴏᴋᴀʏ, ɪ ᴡɪʟʟ ɴᴏᴛ ᴜɴᴘɪɴ ᴀɴʏᴛʜɪɴɢ.**"
-    else:
-        try:
-            msg_id = int(msg_id)
-            await client.unpin_chat_message(chat_id, msg_id)
-            textt = "**✅ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ ᴜɴᴘɪɴɴᴇᴅ!**"
-        except:
-            textt = "**⚠ ғᴀɪʟᴇᴅ ᴛᴏ ᴜɴᴘɪɴ ᴍᴇssᴀɢᴇ.**"
+    if data.startswith("unpin="):
+        action = data.split("=")[1]
 
-    await CallbackQuery.message.edit_caption(
-        textt,
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="ᴅᴇʟᴇᴛᴇ", callback_data="delete_btn=admin")]]
+        if action == "yes":
+            await client.unpin_all_chat_messages(chat_id)
+            textt = "**✅ ᴀʟʟ ᴘɪɴɴᴇᴅ ᴍᴇssᴀɢᴇs ʜᴀᴠᴇ ʙᴇᴇɴ ᴜɴᴘɪɴɴᴇᴅ!**"
+        elif action == "no":
+            textt = "**❌ ᴏᴋᴀʏ, ɪ ᴡɪʟʟ ɴᴏᴛ ᴜɴᴘɪɴ ᴀɴʏᴛʜɪɴɢ.**"
+        else:
+            try:
+                msg_id = int(action)
+                await client.unpin_chat_message(chat_id, msg_id)
+                textt = "**✅ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ ᴜɴᴘɪɴɴᴇᴅ!**"
+            except:
+                textt = "**⚠ ғᴀɪʟᴇᴅ ᴛᴏ ᴜɴᴘɪɴ ᴍᴇssᴀɢᴇ.**"
+
+        await CallbackQuery.message.edit_caption(
+            textt,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton(text="ᴅᴇʟᴇᴛᴇ", callback_data="delete_btn=admin")]]
+            )
         )
-    )
 
 @app.on_message(filters.command(["unpinall"]))
 async def unpinall_command(client, message):
