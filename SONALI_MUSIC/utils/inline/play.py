52733eaf96c7c6aa8253305424ba95f5b6866483
sandeep224422 <sandeep.5519051923@ipu.ac.in>
2025-09-11 23:59:30 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -69,7 +69,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
          [
              InlineKeyboardButton(text="< - 𝟤𝟢ˢ", callback_data="seek_backward_20"),
-             InlineKeyboardButton(text="• ᴘʀᴏᴍᴏ •", url=f"https://t.me/m/FFljyYI6NmE9"),
+             InlineKeyboardButton(text="• ᴘʀᴏᴍᴏ •", url=f"https://t.me/brahix"),
              InlineKeyboardButton(text="𝟤𝟢ˢ + >", callback_data="seek_forward_20")
          ],
         [
