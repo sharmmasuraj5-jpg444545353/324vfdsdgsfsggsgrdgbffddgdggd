556b3a13722e92e31b7f20419ec3ec0522fb7f47
Sandeep sharma <rs3268840@gmail.com>
2025-10-26 12:50:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/sandeep224422/brahix",)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Suraj08832/heeriye",)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", "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")
