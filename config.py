b59ce3135d02a00b09d6196099f507ea853941c7
Sandeep sharma <rs3268840@gmail.com>
2025-12-24 17:42:10 +0530

    Update Heroku and Git token configuration

--- config.py
+++ config.py
@@ -29,11 +29,9 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Suraj08832/heeriye",)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Suraj08832/hexanerve.git") #abhi change kiya 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
-
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
 # -------------------- [ LOGGER & LINKS ] --------------------
 
 LOGGER_ID = int(getenv("LOGGER_ID", -1002933652916))
